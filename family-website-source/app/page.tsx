679d34c909bcc9838929996661eb98dafd9e7193
Karnell Holmes <154283253+karnell2324@users.noreply.github.com>
2026-08-23 13:40:44 -0400

    Delete family-website-source directory

--- family-website-source/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { requireChatGPTUser, chatGPTSignOutPath } from "./chatgpt-auth";
-import FamilyPortal from "./family-portal";
-
-export const dynamic = "force-dynamic";
-
-export default async function Home() {
-  const user = await requireChatGPTUser("/");
-  return <FamilyPortal user={{ name: user.displayName, email: user.email }} signOutPath={chatGPTSignOutPath("/")} />;
-}
